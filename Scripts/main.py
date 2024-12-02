dc983856cb56eab6655fcfa06a44dc7673367cf4
piyush-1871 <piyush1871b@gmail.com>
2024-12-02 23:07:12 +0530

    Added Home image

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,3 +1,6 @@
 import streamlit as st
 import mysql.connector
 st.title("CUSTOMER MANAGEMENT SYSTEM")
+choice = st.sidebar.selectbox("My Menu", ("Home", "User", "Admin"))
+if(choice == "Home"):
+    st.image("https://chisellabs.com/glossary/wp-content/uploads/2023/05/f17fea9f-e83a-4f9b-924c-508d29a53f24.png")
