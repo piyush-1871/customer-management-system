2a928cfc2a3d08e1b80bdd5fd7f633738588dbb2
piyush-1871 <piyush1871b@gmail.com>
2024-12-03 00:08:57 +0530

    Add New Customer Feature added when admin logins

--- Scripts/main.py
+++ Scripts/main.py
@@ -31,4 +31,30 @@ elif(choice == 'Admin'):
             df_no_index = df.to_records(index=False)
             #Display dataframe
             st.dataframe(df_no_index)
-                
+        elif(choice2 == "Add New Customer"):
+            #Input Fields
+            cust_id = st.number_input("Enter the Customer ID: ", min_value = 1, step = 1, format = '%d')
+            first_name = st.text_input("Enter the first name: ")
+            last_name = st.text_input("Enter the last name: ")    
+            age = st.number_input("Enter the Customer ID: ", min_value = 18, max_value = 100, step = 1, format = '%d')     
+            gender = st.selectbox("Enter the gender (M/F): ", ['M', 'F'])
+            phone = st.text_input("Enter the phone no. : ")
+            address = st.text_input("Enter the address : ")
+            email_id = st.text_input("Enter the email : ")
+            user_pass = st.text_input("Enter the password for Customer: ")
+            btn3 = st.button("Submit")
+            if(btn3):
+                mydb = mysql.connector.connect(host="localhost", user="root", password="Piyush@2002", database="cms")
+                c = mydb.cursor()
+                #Check if customer id already exists
+                c.execute("SELECT COUNT(*) FROM customers WHERE cust_id = %s", (cust_id,))
+                if(c.fetchone()[0] > 0):
+                    st.error("Customer ID already exists. Please choose different Customer ID.")
+                else:
+                    #Insert the new user into user table first
+                    c.execute("INSERT INTO user (email_id, user_pass) VALUES (%s, %s)", (email_id, user_pass))
+                    #Insert into customers table
+                    c.execute("INSERT INTO customers (cust_id, first_name, last_name, age, gender, phone, address, email_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (cust_id, first_name, last_name, age, gender, phone, address, email_id))
+                    
+                    mydb.commit()
+                    st.header("Customer Added Successfully!")
