dfa5afbd25f2fe625de58ee8f50d440cfecd10b8
piyush-1871 <piyush1871b@gmail.com>
2024-12-02 23:24:45 +0530

    Admin Login feature added

--- Scripts/main.py
+++ Scripts/main.py
@@ -4,3 +4,24 @@ st.title("CUSTOMER MANAGEMENT SYSTEM")
 choice = st.sidebar.selectbox("My Menu", ("Home", "User", "Admin"))
 if(choice == "Home"):
     st.image("https://chisellabs.com/glossary/wp-content/uploads/2023/05/f17fea9f-e83a-4f9b-924c-508d29a53f24.png")
+elif(choice == 'Admin'):
+    if 'alogin' not in st.session_state:
+        st.session_state['alogin'] = False
+    admin_id = st.text_input("admin id: ")
+    admin_pass = st.text_input("admin password: ")  
+    btn = st.button("Login")  
+    if btn:
+        mydb = mysql.connector.connect(host="localhost", user="root", password="Piyush@2002", database="cms")
+        c = mydb.cursor()
+        c.execute("select * from admin")
+        for r in c:
+            if(r[0] == admin_id and r[1] == admin_pass):
+                st.session_state['alogin'] = True
+                break
+    if(not st.session_state['alogin']):
+        st.write("Incorrect ID or Password!")
+        
+    if(st.session_state['alogin']):
+        st.write("Login Successfull!")
+                    
+                
